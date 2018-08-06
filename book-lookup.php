67922af21843539e6ae02ebca437f46f79c1e9cc
Bradstreet <bradstreetrand@gmail.com>
2018-08-06 13:43:35 -0400

    Added Google Books search through user input using AJAX

--- book-lookup.php
+++ book-lookup.php
@@ -1,6 +1,6 @@
 <?php
 
-$url = "https://www.googleapis.com/books/v1/volumes?q=ISBN:9781524741723";
+$url = "https://www.googleapis.com/books/v1/volumes?q=ISBN:9781524741723&maxResults=3";
 
 // Initialize cURL session
 $cSession = curl_init(); 
@@ -12,7 +12,7 @@ curl_setopt($cSession, CURLOPT_RETURNTRANSFER, true);
 //curl_setopt($cSession, CURLOPT_HEADER, false);
 
 // Esecute cURL session
-$result = curl_exec($cSession);
+$rawResult = curl_exec($cSession);
 
 
 if(curl_exec($cSession) === false)
@@ -27,6 +27,17 @@ else
 // Close the cURL session
 curl_close($cSession);
 
-echo $result;
+$bookLookupArray = json_decode($rawResult, TRUE);
+
+$result = array();
+
+for ($i=0; $i < 3 ; $i++) { 
+	$result[$i] = array(
+		"googleId" => $bookLookupArray['items'][$i]['id'], 
+		"volumeInfo" => $bookLookupArray['items'][$i]['volumeInfo'],
+	);
+}
+
+var_dump($result);
 
 ?>
