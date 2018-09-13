259116008b1eaf9f8e71f69c8982117c49e1ebe7
Bradstreet <bradstreetrand@gmail.com>
2018-09-12 20:18:32 -0400

    Set up student page to show student number and all books checked out

--- studenthome.php
+++ studenthome.php
@@ -31,11 +31,14 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
                 mysqli_stmt_bind_result($stmt, $col1);
                 // Fetches results
                 $result = mysqli_stmt_fetch($stmt);
-                $student = $col1
+                $student = $col1;
                 // Returns student number
                 echo "Student Number:" . $student;
 
-                $sql1 = "SELECT book.title, book.author, book.thumbnail, book.description, reading_log.start, reading_log.finish, reading_log.rating FROM student INNER JOIN reading_log ON student.student = reading_log.student INNER JOIN book ON reading_log.book = book.book WHERE student.student = ?"
+                // Close statement
+                mysqli_stmt_close($stmt);
+
+                $sql1 = "SELECT book.title, book.author, book.thumbnail, book.description, reading_log.start, reading_log.finish, reading_log.rating FROM student INNER JOIN reading_log ON student.student = reading_log.student INNER JOIN book ON reading_log.book = book.book WHERE student.student = ?";
                 if ($stmt1 = mysqli_prepare($link, $sql1)) {
                     // Bind variables to the prepared statement as parameters
                     mysqli_stmt_bind_param($stmt1, 'i', $param_student);
@@ -48,18 +51,34 @@ if(!isset($_SESSION['username']) || empty($_SESSION['username'])){
                         // Bind variables to prepared statement
                         mysqli_stmt_bind_result($stmt1, $col1, $col2, $col3, $col4, $col5, $col6, $col7);
                         // Fetches results
-                        $result = mysqli_stmt_fetch($stmt);
+                        $result = mysqli_stmt_fetch($stmt1);
 
+                        while ($result1 = mysqli_stmt_fetch($stmt1)) {
+                            $searchResult[] =  ["title" => $col1, "author" => $col2, "thumbnail" => $col3, "description" => $col4, "start" => $col5, "finish" => $col6, "rating" => $col7];
+                        }
+                        
+                        foreach ($searchResult as $book) {
+                            echo "</br>" . $book["title"] . $book["author"] . $book["thumbnail"] . $book["description"] . $book["start"] . $book["finish"] . $book["rating"] . "</br>";
+                        }
+
+                        // Close statement
+                        mysqli_stmt_close($stmt1);
+                    } else {
+                        echo "SQL statement 1 not executed" . mysqli_error($link);
+                    }
+                } else {
+                    echo "SQL statement 1 not prepared" . mysqli_error($link);
                 }
-
             } else {
-                echo "SQL statement not executed";
+                echo "SQL statement not executed" . mysqli_error($link);
             }
         } else {
-            echo "SQL statement not prepared";
+            echo "SQL statement not prepared" . mysqli_error($link);
         }
-        // Close statement
-        mysqli_stmt_close($stmt);
+        
+
+// Close connection
+mysqli_close($link);
 
 ?>
 
